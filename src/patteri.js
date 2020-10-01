52eabb745933f943f1aebcc8a2a1fe41984e9b1f
Jari Mäkelä <jari@iki.fi>
2020-10-01 21:04:55 +0300

    Bootstrap code

---
+++ src/patteri.js
@@ -0,0 +1,6 @@
+console.log("ehlo")
+const tag = document.createElement("p");
+const text = document.createTextNode("hello, universe");
+tag.appendChild(text);
+const element = document.getElementById("patteri");
+element.appendChild(tag);
