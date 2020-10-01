ee5f12d2c88b1e6365df4d9dd3e027fdcff668ba
Jari Mäkelä <jari@iki.fi>
2020-10-01 21:31:33 +0300

    Config eslint

--- src/patteri.js
+++ src/patteri.js
@@ -1,6 +1,5 @@
-console.log("ehlo")
-const tag = document.createElement("p");
-const text = document.createTextNode("hello, universe");
-tag.appendChild(text);
-const element = document.getElementById("patteri");
-element.appendChild(tag);
+const tag = document.createElement("p")
+const text = document.createTextNode("hello, universe")
+tag.appendChild(text)
+const element = document.getElementById("patteri")
+element.appendChild(tag)
