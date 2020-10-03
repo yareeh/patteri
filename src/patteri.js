19fc780b1cdedc4323b3e010a2030a247a71c8d1
Jari Mäkelä <jari@iki.fi>
2020-10-03 18:26:50 +0300

    Reload every minute

--- src/patteri.js
+++ src/patteri.js
@@ -1,17 +1,23 @@
 const patteri = document.getElementById("patteri")
 const config = document.getElementById("config")
 const showConfig = document.getElementById("showConfig")
+let running
+let token
+let repo
 
 // eslint-disable-next-line no-unused-vars
 function enableConfig() {
   "use strict"
 
+  setStopped()
   config.removeAttribute("class")
   showConfig.setAttribute("class", "hide")
 }
 
-async function getBuilds(token, repo) {
+async function getBuilds() {
   "use strict"
+
+  if (!running) return
 
   config.setAttribute("class", "hide")
   showConfig.removeAttribute("class")
@@ -22,15 +28,24 @@ async function getBuilds(token, repo) {
   const response = await fetch(
     `https://api.github.com/repos/${repo}/actions/workflows`,
     { headers }
-  )
+  ).catch((err) => {
+    setStopped()
+    // eslint-disable-next-line no-console
+    console.log(err)
+  })
   const workflows = (await response.json()).workflows
 
   const height = (window.innerHeight * 0.9) / (workflows.length / 2)
+
+  Array.from(document.getElementsByClassName("build")).forEach((b) =>
+    b.remove()
+  )
 
   workflows.forEach((w) => {
     const img = document.createElement("img")
     img.setAttribute("src", w.badge_url)
     img.setAttribute("height", `${height}px`)
+    img.setAttribute("class", "build")
     patteri.appendChild(img)
   })
 }
@@ -39,14 +54,17 @@ async function getBuilds(token, repo) {
 async function getBuildsUsingConfig() {
   "use strict"
 
-  const token = document.getElementById("token").value
-  const repo = document.getElementById("repo").value
-  await getBuilds(token, repo)
+  token = document.getElementById("token").value
+  repo = document.getElementById("repo").value
+  setRunning()
+  await getBuilds()
 }
 
+setStopped()
+
 const searchParams = new URLSearchParams(window.location.search)
-const token = searchParams.get("token")
-const repo = searchParams.get("repo")
+token = searchParams.get("token")
+repo = searchParams.get("repo")
 
 if (
   window.location.hostname === "localhost" &&
@@ -55,5 +73,18 @@ if (
   repo !== null &&
   repo !== ""
 ) {
+  setRunning()
   getBuilds(token, repo)
 }
+
+function setRunning() {
+  "use strict"
+  running = true
+}
+
+function setStopped() {
+  "use strict"
+  running = false
+}
+
+setInterval(getBuilds, 60000)
