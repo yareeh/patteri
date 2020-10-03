4d26cc270c0ba4366f948b484353857b3f2de1f1
Jari Mäkelä <jari@iki.fi>
2020-10-03 16:58:58 +0300

    Make configurable

--- src/patteri.js
+++ src/patteri.js
@@ -1,9 +1,17 @@
 const patteri = document.getElementById("patteri")
 
+// eslint-disable-next-line no-unused-vars
 async function getBuilds() {
   "use strict"
+
+  const headers = new Headers({
+    Authorization: `token ${document.getElementById("token").value}`,
+  })
   const response = await fetch(
-    "https://api.github.com/repos/yareeh/patteri/actions/workflows"
+    `https://api.github.com/repos/${
+      document.getElementById("repo").value
+    }/actions/workflows`,
+    { headers }
   )
   const workflows = (await response.json()).workflows
   workflows.forEach((w) => {
@@ -12,5 +20,3 @@ async function getBuilds() {
     patteri.appendChild(img)
   })
 }
-
-getBuilds()
