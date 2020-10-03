97c0887a9ed219e61fbabff59ac8d118df94e185
Jari Mäkelä <jari@iki.fi>
2020-10-03 17:31:27 +0300

    Fill window

--- src/patteri.js
+++ src/patteri.js
@@ -24,9 +24,13 @@ async function getBuilds(token, repo) {
     { headers }
   )
   const workflows = (await response.json()).workflows
+
+  const height = (window.innerHeight * 0.9) / (workflows.length / 2)
+
   workflows.forEach((w) => {
     const img = document.createElement("img")
     img.setAttribute("src", w.badge_url)
+    img.setAttribute("height", `${height}px`)
     patteri.appendChild(img)
   })
 }
