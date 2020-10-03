20ddd7d464d71c608d5d86872f8acc9995526b17
Jari Mäkelä <jari@iki.fi>
2020-10-03 17:12:52 +0300

    Allow using command line parameters
    Only if host is `localhost`. Because security.

--- src/patteri.js
+++ src/patteri.js
@@ -1,16 +1,13 @@
 const patteri = document.getElementById("patteri")
 
-// eslint-disable-next-line no-unused-vars
-async function getBuilds() {
+async function getBuilds(token, repo) {
   "use strict"
 
   const headers = new Headers({
-    Authorization: `token ${document.getElementById("token").value}`,
+    Authorization: `token ${token}`,
   })
   const response = await fetch(
-    `https://api.github.com/repos/${
-      document.getElementById("repo").value
-    }/actions/workflows`,
+    `https://api.github.com/repos/${repo}/actions/workflows`,
     { headers }
   )
   const workflows = (await response.json()).workflows
@@ -19,4 +16,27 @@ async function getBuilds() {
     img.setAttribute("src", w.badge_url)
     patteri.appendChild(img)
   })
+}
+
+// eslint-disable-next-line no-unused-vars
+async function getBuildsUsingConfig() {
+  "use strict"
+
+  const token = document.getElementById("token").value
+  const repo = document.getElementById("repo").value
+  await getBuilds(token, repo)
+}
+
+const searchParams = new URLSearchParams(window.location.search)
+const token = searchParams.get("token")
+const repo = searchParams.get("repo")
+
+if (
+  window.location.hostname === "localhost" &&
+  token !== null &&
+  token !== "" &&
+  repo !== null &&
+  repo !== ""
+) {
+  getBuilds(token, repo)
 }
